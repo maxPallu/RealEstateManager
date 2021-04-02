a87ae0d4d246b511d83e5decc17b520a7148b489
MaxPallu <maximilien2776@gmail.com>
2021-04-02 16:08:38 +0200

    Mise en place Map Lite mode dans DetailActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/DetailActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/DetailActivity.java
@@ -1,16 +1,36 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Intent;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.openclassrooms.realestatemanager.DI.DI;
+import com.openclassrooms.realestatemanager.Injection.Injection;
+import com.openclassrooms.realestatemanager.Injection.ViewModelFactory;
+import com.openclassrooms.realestatemanager.models.Result;
+import com.openclassrooms.realestatemanager.util.ApiCalls;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback, ApiCalls.Callbacks {
 
     private TextView detailSurface;
     private TextView detailRoom;
@@ -25,6 +45,9 @@ public class DetailActivity extends AppCompatActivity {
     private String estateCity;
     private String estateAdress;
 
+    private GoogleMap map;
+    private Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +58,20 @@ public class DetailActivity extends AppCompatActivity {
         detailCity = findViewById(R.id.locationCity);
         detailAdress = findViewById(R.id.locationAdress);
         detailPicture = (ImageView) findViewById(R.id.pictureView);
+
+        back = findViewById(R.id.back);
+
+     //  back.setOnClickListener(new View.OnClickListener() {
+     //      @Override
+     //      public void onClick(View view) {
+     //          finish();
+     //      }
+     //  });
+
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+        GoogleMapOptions options = new GoogleMapOptions().liteMode(true);
 
         Intent intent = getIntent();
 
@@ -51,5 +88,42 @@ public class DetailActivity extends AppCompatActivity {
 
         Uri pictureUri = Uri.parse(estateUri);
         detailPicture.setImageURI(pictureUri);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        getLocation();
+    }
+
+    private void getLocation() {
+        ApiCalls.fetchLocations(this, estateAdress);
+    }
+
+    private void setupMarkers(List<Result> locations) {
+        if(locations != null && locations.isEmpty() == false) {
+            for(int i =0; i<locations.size(); i++) {
+                double lat = locations.get(i).getGeometry().getLocation().getLat();
+                double lng = locations.get(i).getGeometry().getLocation().getLng();
+
+                LatLng latLng = new LatLng(lat, lng);
+                map.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title(estateCity));
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13f));
+            }
+        }
+    }
+
+    @Override
+    public void onResponse(@Nullable List<Result> locations) {
+        setupMarkers(locations);
+    }
+
+    @Override
+    public void onFailure() {
+
     }
 }
